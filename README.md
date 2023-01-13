b4d3c89685d42855928a77b7f0359beee278ddef
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-01-13 11:39:16 +0200

    app tiedostossa esimerkkejä

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 Sovellus tehdään MVC-mallin mukaisesti ja tähän backendiin toteutetaan 
 **Model** ja **Controller** osiot.
+
+## Middleware
+
+On funktio, joka suoritetaan ilman että sitä tarvitsee "kutsua".
+Sen tunnistaa sanasta use.
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',
+    function(request, response){
+        response.send("Express REST API");
+    }
+);
+
+app.use(
+    function(request,response,next){
+        console.log("Tämä on Middleware 1");
+        next();
+    }
+);
+
+app.get('/example1',
+    function(request,response){
+        response.send('I am example');
+        console.log('I am example hei');
+    }
+);
+
+app.use(
+    function(request,response,next){
+        console.log("Tämä on Middleware 2");
+        next();
+    }
+);
+
+app.get('/sayhello/:fname',
+    function(request,response){
+        console.log("test");
+        let fn=request.params.fname;
+        response.send("Terve "+fn);
+    }
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+module.exports =app;
